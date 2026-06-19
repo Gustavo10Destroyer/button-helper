9aa8249b104b91dfb2e5d06b94385741b611b26f
Gustavo Santos <contaderecuperacoes2018@gmail.com>
2026-06-19 17:33:58 -0300

    style: format code

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
 export {
-  sendInteractiveButtonsBasic,
-  sendInteractiveButtonsBasic as sendButtons,
-  sendInteractiveMessage,
+    sendInteractiveButtonsBasic,
+    sendInteractiveButtonsBasic as sendButtons,
+    sendInteractiveMessage,
 } from './services/interactiveMessageService.js';
 export { getButtonArgs, getButtonType } from './services/buttonNodeService.js';
 export { convertToInteractiveMessage } from './services/messageContentService.js';
-export { buildInteractiveButtons, mapAuthoringButton } from './mappers/buttonMapper.js';
 export {
-  deserializeNativeFlowButton,
-  parseButtonParamsJson,
-  serializeNativeFlowButton,
-  toExternalButtonParams,
+    buildInteractiveButtons,
+    mapAuthoringButton,
+} from './mappers/buttonMapper.js';
+export {
+    deserializeNativeFlowButton,
+    parseButtonParamsJson,
+    serializeNativeFlowButton,
+    toExternalButtonParams,
 } from './mappers/buttonParamsMapper.js';
 export { InteractiveValidationError } from './errors/InteractiveValidationError.js';
 export {
-  examplePayloads,
-  validateAuthoringButtons,
-  validateInteractiveMessageContent,
-  validateSendButtonsPayload,
-  validateSendInteractiveMessagePayload,
+    examplePayloads,
+    validateAuthoringButtons,
+    validateInteractiveMessageContent,
+    validateSendButtonsPayload,
+    validateSendInteractiveMessagePayload,
 } from './validators/buttonValidators.js';
 export * from './types/buttonParams.js';
 export type * from './types/common.js';
